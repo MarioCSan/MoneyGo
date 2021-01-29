de9ea110fed85594f7ff66ecc8c55a6184aad4de
Mario Canales <Mario.Canales@tajamar365.com>
2021-01-29 09:51:41 +0100

    Se añade las vistas del landing y se añaden los metodos al controlador. Se crean los directorios(vacios) para el resto de la estructura del proyecto.

---
+++ MoneyGo/Controllers/LandingController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoneyGo.Controllers
+{
+    public class LandingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(String email, String password)
+        {
+            return View();
+        }
+
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult LogIn(String email, String password)
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(String nombre, String username, String password)
+        {
+            return View();
+        }
+    }
+}
--- MoneyGo/Startup.cs
+++ MoneyGo/Startup.cs
@@ -50,7 +50,7 @@ namespace MoneyGo
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Landing}/{action=Index}/{id?}");
             });
         }
     }
