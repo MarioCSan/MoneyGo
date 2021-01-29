21f1c5e28d6b52e31e4f3abbe2fee78ce7727d74
Mario Canales <Mario.Canales@tajamar365.com>
2021-01-29 10:03:27 +0100

    Se mapea el modelo de 'Transacciones' de manera provisional ya que falta añadir campos a las tablas. Se añaden ficheros a gitignore.

---
+++ MoneyGo/Models/Transacciones.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoneyGo.Models
+{
+    [Table("TRANSACCIONES")]
+    public class Transacciones
+    {
+        [Key]
+        [Column("IDTRANSACCIONE")]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int IdTransaccion { get; set; }
+
+        [Column("IDUSUARIO")]
+        public int IdUsuario { get; set; }
+
+
+        [Column("CANTIDAD")]
+        public int Cantidad { get; set; }
+        //MAPEAR A FLOAT
+
+        [Column("TIPOTRANSACCION")]
+        public String TipoTransaccion { get; set; }
+
+        [Column("FECHATRANSACCION")]
+        public DateTime FechaTransaccion { get; set; }
+    }
+}
