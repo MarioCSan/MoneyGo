8fd50dd645acde9d1788f618a7d4bc22d7b523fb
Mario <canalessanchezmario@gmail.com>
2021-02-04 18:22:36 +0100

    Se añade código al 'UploadService' y al 'PathProvider'

--- MoneyGo/Helpers/PathProvider.cs
+++ MoneyGo/Helpers/PathProvider.cs
@@ -1,6 +1,44 @@
-﻿namespace MoneyGo.Helpers
+﻿using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoneyGo.Helpers
 {
-    internal class PathProvider
+    public enum Folders
     {
+        Images = 0, Documents = 1, tmp = 2
     }
-}
+
+    public class PathProvider
+    {
+        IWebHostEnvironment env;
+
+        public PathProvider(IWebHostEnvironment env)
+        {
+            this.env = env;
+        }
+
+        //Metodos para devolver la ruta a ficheros
+        public String MapPath(String filename, Folders folder)
+        {
+            String carpeta = ""; //Documents o images
+
+            if (folder == Folders.Documents)
+            {
+                carpeta = "documents";
+            }
+            else if (folder == Folders.Images)
+            {
+                carpeta = "images";
+            }
+            else if (folder == Folders.tmp)
+            {
+                carpeta = "tmp";
+            }
+            String path = Path.Combine(this.env.WebRootPath, carpeta, filename);
+            return path;
+        }
+    }
