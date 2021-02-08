1c192729c20b8f8c421c381a9fc3adc0fc29600c
Mario <canalessanchezmario@gmail.com>
2021-02-08 17:34:17 +0100

    Se reestablece el proyecto desde una copia de seguridad  debido a un problema de sincronización con GitHub.

---
+++ MoneyGo/Data/TransaccionesContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoneyGo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoneyGo.Data
+{
+    public class TransaccionesContext: DbContext
+    {
+        public TransaccionesContext(DbContextOptions<TransaccionesContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Transacciones> Transacciones { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Usuario>().HasAlternateKey(a => a.IdUsuario);
+            modelBuilder.Entity<Transacciones>().HasAlternateKey(a => a.IdUsuario);
+        }
+    }
+}
