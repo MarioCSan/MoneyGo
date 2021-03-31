9457968e2e6acff46dc23cf0d3138649ea017d68
Mario <canalessanchezmario@gmail.com>
2021-03-31 10:43:53 +0200

    Se añaden los proyectos de la migración a una API y del NuGet.

---
+++ MoneyGoAPI/MoneyGoAPI/Controllers/TransaccionesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MoneyGoAPI.Models;
+using MoneyGoAPI.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoneyGoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransaccionesController : ControllerBase
+    {
+        RepositoryTransacciones repo;
+        public TransaccionesController(RepositoryTransacciones repo)
+        {
+            this.repo = repo;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Transacciones>> GetTransaccionesUsuario(int id)
+        {
+            return this.repo.GetTransacciones(id);
+        }
+
+        
+    }
+}
---
+++ MoneyGoAPI/MoneyGoAPI/Startup.cs
@@ -0,0 +1,85 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using MoneyGo.Helpers;
+using MoneyGoAPI.Data;
+using MoneyGoAPI.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoneyGoAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            String cadena = this.Configuration.GetConnectionString("azuredb");
+
+            services.AddTransient<RepositoryTransacciones>();
+           
+            services.AddDbContext<TransaccionesContext>(options => options.UseSqlServer(cadena));
+
+            services.AddSingleton<MailService>();
+            //services.AddSingleton<UploadService>();
+            services.AddSingleton<PathProvider>();
+
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc(name: "v1", new OpenApiInfo
+                {
+                    Title = "API CRUD Departamentos Core",
+                    Version = "v1",
+                    Description = "OPen API"
+                });
+            });
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint(
+                    url: "/swagger/v1/swagger.json", name: "Api v1");
+                options.RoutePrefix = "";
+            });
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
