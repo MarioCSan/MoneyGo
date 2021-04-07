cd2120080672b722d10fed48d7b64f957bd1e6b9
Mario <canalessanchezmario@gmail.com>
2021-04-07 19:13:30 +0200

    Se crea una version inicial del nuegt y se publica. Se añade autorizacion al controlador de Usuarios.

---
+++ NuGetProyecto/NuGetProyecto/Models/Transacciones.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace NuGetProyecto.Models
+{
+    [Table("TRANSACCIONES")]
+    public class Transacciones
+    {
+        [Key]
+        [Column("IDTRANSACCION")]
+        public int IdTransaccion { get; set; }
+
+        [ForeignKey("IDUSUARIO")]
+        [Column("IDUSUARIO")]
+        public int IdUsuario { get; set; }
+
+
+        [Column("CANTIDAD")]
+        public Double Cantidad { get; set; }
+
+        [Column("TIPOTRANSACCION")]
+        public String TipoTransaccion { get; set; }
+
+        [Column("FECHATRANSACCION")]
+        public DateTime FechaTransaccion { get; set; }
+
+        [Column("CONCEPTO")]
+        public String Concepto { get; set; }
+    }
+}
